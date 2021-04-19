641b27059d85bf483f9637e87cd7f57cd6fdf860
goodGid <hello.goodgid@gmail.com>
2021-04-19 09:28:51 +0900

    Implement QueryDSL
    - Add JpaConfig
    - Add QueryDSL dependency

--- module-api/src/main/java/dev/be/service/TestService.java
+++ module-api/src/main/java/dev/be/service/TestService.java
@@ -1,6 +1,13 @@
 package dev.be.service;
 
+import static dev.be.domain.QMember.member;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import dev.be.domain.Member;
 import dev.be.repository.MemberRepository;
@@ -13,8 +20,21 @@ import lombok.extern.slf4j.Slf4j;
 public class TestService {
 
     private final MemberRepository memberRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
     public String checkDB() {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(member.id.eq(1L));
+        builder.or(member.id.eq(2L));
+
+        // use QueryDSL
+        List<Member> memberList = jpaQueryFactory.selectFrom(member)
+                                                 .where(builder)
+                                                 .fetch();
+
+        log.info("memberList.size() : {}", memberList.size());
+
         // use CrudRepository interface
         memberRepository.save(Member.builder()
                                     .name("goodGid")
